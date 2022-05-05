2e569ff30b4bb604daaa3b6e0a7be42203a5d30d
kibuniverse <50582439+kibuniverse@users.noreply.github.com>
2022-05-05 14:30:55 +0800

    鎻fe瀹屾垚鍓鍩烘湰閰嶇疆

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,16 @@
 import * as React from 'react';
 
-export default function () {
-  return <div>this is a ui component</div>;
-}
+type TabsProps = {
+  title?: string;
+};
+
+const index: React.FC<TabsProps> = props => {
+  const { title = '' } = props;
+  console.log(props.children);
+  const [activeIndex, setActiveIndex] = React.useState(0);
+  const children = React.Children.toArray(props.children);
+  console.log(children);
+  return <div>{title}</div>;
+};
+
+export default index;
--- stories/index.stories.tsx
+++ stories/index.stories.tsx
@@ -1,7 +1,12 @@
-const Component = () => <div>this is a Story Componernt</div>;
+import Tab from '@/index';
 
-export const YourStory = () => <Component />;
+export const TabComponent = () => (
+  <Tab title="标题">
+    <div key="first">第一个tab</div>
+    <div key="second">第二个tab</div>
+  </Tab>
+);
 
 export default {
-  title: 'Your Stories',
+  title: 'components',
 };
